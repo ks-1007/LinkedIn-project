fecaac17413e5e192cecf137023393978c1ee037
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-28 20:37:55 +0530

    worked on about section

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,3 +1,4 @@
+import { About } from "../Components/ProfilePage/About"
 import { Hero } from "../Components/ProfilePage/Hero"
 import styles from "../Components/ProfilePage/styles/ProfilePage.module.css"
 export function ProfilePage() {
@@ -5,6 +6,7 @@ export function ProfilePage() {
     <div className={styles.rootCont}>
       <div className={styles.leftSection}>
         <Hero />
+        <About />
       </div>
       <div className={styles.rightSection}></div>
     </div>
