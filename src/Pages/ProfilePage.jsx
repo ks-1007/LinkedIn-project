186fcc4f40055037aa08a7f1dae69576a6905b59
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-28 23:08:12 +0530

    worked on skills section

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { About } from "../Components/ProfilePage/About"
 import { Education } from "../Components/ProfilePage/Education"
 import { Hero } from "../Components/ProfilePage/Hero"
+import { Skills } from "../Components/ProfilePage/Skills"
 import styles from "../Components/ProfilePage/styles/ProfilePage.module.css"
 export function ProfilePage() {
   return (
@@ -9,6 +10,7 @@ export function ProfilePage() {
         <Hero />
         <About />
         <Education />
+        <Skills />
       </div>
       <div className={styles.rightSection}></div>
     </div>
