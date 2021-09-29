1af8eab461b7d99b807dd3f1752aadc56824fc90
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-29 22:07:07 +0530

    added basic setup for server

---
+++ src/Server/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  connections: [{ type: mongoose.Schema.Types.ObjectId, required: false }],
+  profile_pic: { type: String, required: false },
+  background_pic: { type: String, required: false },
+  location: { type: String, required: false },
+  about: { type: String, required: false },
+  education: [{ type: String, required: false }],
+  licenses_certifications: [{ type: String, required: false }],
+  skill: [{ type: String, required: false }],
+  accomplishments: [{ type: String, required: false }],
+  requests: [{ type: String, required: false }],
+  invites: [{ type: String, required: false }],
+})
+
+module.exports = mongoose.model("user", userSchema)
