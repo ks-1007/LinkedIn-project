40e428a0fbf5c1bc916e80177f7550b4e17e5649
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-29 13:00:19 +0530

    worked on interests section

---
+++ src/Components/ProfilePage/Interests.jsx
@@ -0,0 +1,160 @@
+import styles from "./styles/ProfilePage.module.css"
+export function Interests() {
+  return (
+    <>
+      <div className={styles.interestsCont}>
+        <p>Interests</p>
+        <div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C5603AQHHUuOSlRVA1w/profile-displayphoto-shrink_100_100/0/1579726624860?e=1638403200&v=beta&t=AiPJvGKkvozexU0QQA9rmWDlxcX8_ViIuJlSfP4swwY"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>
+                Satya Nadella{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path
+                    d="M14 0H2a1 1 0 00-1 1v15l3-2h10a1 1 0 001-1V1a1 1 0 00-1-1zM5 12H3V5h2zM4 4.2A1.27 1.27 0 012.75 3a1.25 1.25 0 012.5 0A1.27 1.27 0 014 4.2zm9 7.8h-2V8.73c0-.79-.18-1.93-1.34-1.93A1.74 1.74 0 008 8.61V12H6V5h1.85v1a2.72 2.72 0 012.29-1.2C12.27 4.8 13 6.09 13 8.29z"
+                    fill="#0a66c2"
+                  ></path>
+                </svg>
+              </p>
+              <p>Chairman and CEO at Microsoft</p>
+              <p>9,346,969 followers</p>
+            </div>
+          </div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQHh6_Wth5f3rQ/profile-displayphoto-shrink_100_100/0/1625181962692?e=1638403200&v=beta&t=yQN_YZ3FlWcPNXOUaSJFvqqeAQ9mSlL3bKGQcKg5UfI"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>
+                Richard Branson{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path
+                    d="M14 0H2a1 1 0 00-1 1v15l3-2h10a1 1 0 001-1V1a1 1 0 00-1-1zM5 12H3V5h2zM4 4.2A1.27 1.27 0 012.75 3a1.25 1.25 0 012.5 0A1.27 1.27 0 014 4.2zm9 7.8h-2V8.73c0-.79-.18-1.93-1.34-1.93A1.74 1.74 0 008 8.61V12H6V5h1.85v1a2.72 2.72 0 012.29-1.2C12.27 4.8 13 6.09 13 8.29z"
+                    fill="#0a66c2"
+                  ></path>
+                </svg>
+              </p>
+              <p>Founder at Virgin Group</p>
+              <p>19,414,522 followers</p>
+            </div>
+          </div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C5603AQHrqRduphMBtg/profile-displayphoto-shrink_100_100/0/1610395405056?e=1638403200&v=beta&t=hg6FpneeKqp61NyX8A6CR8n7DaSa2g3BiPZQvTOQ9o4"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>
+                Jeff Immelt{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path
+                    d="M14 0H2a1 1 0 00-1 1v15l3-2h10a1 1 0 001-1V1a1 1 0 00-1-1zM5 12H3V5h2zM4 4.2A1.27 1.27 0 012.75 3a1.25 1.25 0 012.5 0A1.27 1.27 0 014 4.2zm9 7.8h-2V8.73c0-.79-.18-1.93-1.34-1.93A1.74 1.74 0 008 8.61V12H6V5h1.85v1a2.72 2.72 0 012.29-1.2C12.27 4.8 13 6.09 13 8.29z"
+                    fill="#0a66c2"
+                  ></path>
+                </svg>
+              </p>
+              <p>Venture Partner, New Enterprise Associates</p>
+              <p>582,832 followers</p>
+            </div>
+          </div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C510BAQFrwCTLHGW7PQ/company-logo_100_100/0/1553076881382?e=1640822400&v=beta&t=BuEMtsuCvIF0OV7h0mRFfmagnAcbzZUPe7Ep4oCn71E"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>Zomato </p>
+              <p>596,678 followers</p>
+            </div>
+          </div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C5603AQFw3nf9p9UPSA/profile-displayphoto-shrink_100_100/0/1608094037056?e=1638403200&v=beta&t=nhc6ud4r7Ks0jIkgy-1K7nBsraHhsuVrWFE8GLZT3qY"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>
+                Anand C.{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path
+                    d="M14 0H2a1 1 0 00-1 1v15l3-2h10a1 1 0 001-1V1a1 1 0 00-1-1zM5 12H3V5h2zM4 4.2A1.27 1.27 0 012.75 3a1.25 1.25 0 012.5 0A1.27 1.27 0 014 4.2zm9 7.8h-2V8.73c0-.79-.18-1.93-1.34-1.93A1.74 1.74 0 008 8.61V12H6V5h1.85v1a2.72 2.72 0 012.29-1.2C12.27 4.8 13 6.09 13 8.29z"
+                    fill="#0a66c2"
+                  ></path>
+                </svg>
+              </p>
+              <p>Executive Vice President at Five9</p>
+              <p>342,159 followers</p>
+            </div>
+          </div>
+          <div className={styles.interestsDiv}>
+            <div>
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQGwfpy4SZTEUg/company-logo_100_100/0/1625167182161?e=1640822400&v=beta&t=qfP-61Ydkd9FVM8KBl1sfae4s-ZHbDiENQJnq86pgOc"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <p>McKinsey & Company </p>
+              <p>Chairman and CEO at Microsoft</p>
+              <p>4,134,609 followers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.seeAllDiv}>See all</div>
+    </>
+  )
+}
--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { About } from "../Components/ProfilePage/About"
 import { Dashboard } from "../Components/ProfilePage/Dashboard"
 import { Education } from "../Components/ProfilePage/Education"
 import { Hero } from "../Components/ProfilePage/Hero"
+import { Interests } from "../Components/ProfilePage/Interests"
 import { Skills } from "../Components/ProfilePage/Skills"
 import styles from "../Components/ProfilePage/styles/ProfilePage.module.css"
 export function ProfilePage() {
@@ -13,6 +14,7 @@ export function ProfilePage() {
         <About />
         <Education />
         <Skills />
+        <Interests />
       </div>
       <div className={styles.rightSection}></div>
     </div>
