76c81ffbef00cd83b44024e969aeccbe27e03398
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-29 10:20:50 +0530

    worked on dashboard section

--- src/Components/ProfilePage/Dashboard.jsx
+++ src/Components/ProfilePage/Dashboard.jsx
@@ -1,4 +1,8 @@
 import styles from "./styles/ProfilePage.module.css"
+import { FaSatelliteDish } from "react-icons/fa"
+import { FaUserFriends } from "react-icons/fa"
+import { BiMoney } from "react-icons/bi"
+import { BsBookmarkFill } from "react-icons/bs"
 export function Dashboard() {
   return (
     <div className={styles.dashboardCont}>
@@ -24,6 +28,49 @@ export function Dashboard() {
         <div>
           <p>12</p>
           <p>Search appearances</p>
+        </div>
+      </div>
+      <div className={styles.dashboardExtras}>
+        <div>
+          <div>
+            <FaSatelliteDish className={styles.dashboardIcons} />
+          </div>
+          <div>
+            <p>
+              Creator mode: <span>Off</span>{" "}
+            </p>
+            <p>
+              Grow your audience and get discovered by highlighting content on
+              your profile.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <FaUserFriends className={styles.dashboardIcons} />
+          </div>
+          <div>
+            <p>My Network</p>
+            <p>Manage your connections, events and interests.</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <BiMoney className={styles.dashboardIcons} />
+          </div>
+          <div>
+            <p>Salary insights</p>
+            <p>See how your salary compares to others in the community.</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <BsBookmarkFill className={styles.dashboardIcons} />
+          </div>
+          <div>
+            <p>My items</p>
+            <p>Keep track of your jobs, courses and articles.</p>
+          </div>
         </div>
       </div>
     </div>
