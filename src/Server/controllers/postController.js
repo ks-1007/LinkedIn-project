b7d80afdb46e87630b39dc05f05a407af4ec2ac3
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-30 16:17:16 +0530

    partially worked on userController and postController

--- src/Server/controllers/postController.js
+++ src/Server/controllers/postController.js
@@ -2,4 +2,33 @@ const express = require("express")
 
 const router = express.Router()
 
+const Post = require("../models/post.model")
+
+const authenticate = require("../middlewares/authenticate")
+
+// getting feeds of connections
+router.get("", authenticate, async function (req, res) {
+  // getting current user from authenticate middleware
+  const { user } = req.user
+  const { connections } = user
+  const posts = await Post.find({
+    $or: [{ user: user._id }, { user: { $in: connections } }],
+  })
+    .lean()
+    .exec()
+
+  res.send(200).json({ posts })
+})
+
+// posting a post
+router.post("", async function (req, res) {
+  // getting user from authenticate middleware
+  const { user } = req.user
+  let body = req.body
+  body.user = user._id
+  const post = await Post.create(body)
+
+  return res.status(201).json({ post })
+})
+
 module.exports = router
--- src/Server/controllers/userController.js
+++ src/Server/controllers/userController.js
@@ -1,5 +1,34 @@
 const express = require("express")
+const jwt = require("jsonwebtoken")
+
+const newToken = (user) => {
+  return jwt.sign({ user: user }, "masaischool")
+}
+
+const authenticate = require("../middlewares/authenticate")
+
+const User = require("../models/user.model")
 
 const router = express.Router()
+
+// creating a user
+router.post("", async (req, res) => {
+  const user = await User.create(req.body)
+  const token = newToken(user)
+  // we will store this token into redux store
+  return res.status(201).json({ token: token })
+})
+
+// getting all users which are not connection (to show on suggestion list)
+router.get("", authenticate, async (req, res) => {
+  // we will get current user from authenticate middlewares
+  const { user } = req.user
+  const { connections } = user
+  const users = await User.find({ _id: { $nin: connections } })
+    .lean()
+    .exec()
+
+  return res.status(200).send({ users })
+})
 
 module.exports = router
