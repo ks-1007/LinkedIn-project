d3ba5ca7114bfb985746c1bd7b08f93cf9bcb4fe
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-30 11:21:13 +0530

    worked on footer

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { About } from "../Components/ProfilePage/About"
 import { Dashboard } from "../Components/ProfilePage/Dashboard"
 import { Education } from "../Components/ProfilePage/Education"
+import { Footer } from "../Components/ProfilePage/Footer"
 import { Hero } from "../Components/ProfilePage/Hero"
 import { Interests } from "../Components/ProfilePage/Interests"
 import { LinkedInAd } from "../Components/ProfilePage/LinkedInAd"
@@ -9,19 +10,22 @@ import { Skills } from "../Components/ProfilePage/Skills"
 import styles from "../Components/ProfilePage/styles/ProfilePage.module.css"
 export function ProfilePage() {
   return (
-    <div className={styles.rootCont}>
-      <div className={styles.leftSection}>
-        <Hero />
-        <Dashboard />
-        <About />
-        <Education />
-        <Skills />
-        <Interests />
+    <>
+      <div className={styles.rootCont}>
+        <div className={styles.leftSection}>
+          <Hero />
+          <Dashboard />
+          <About />
+          <Education />
+          <Skills />
+          <Interests />
+        </div>
+        <div className={styles.rightSection}>
+          <RightSideTop />
+          <LinkedInAd />
+        </div>
       </div>
-      <div className={styles.rightSection}>
-        <RightSideTop />
-        <LinkedInAd />
-      </div>
-    </div>
+      <Footer />
+    </>
   )
 }
