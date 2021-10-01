be4287dfb87b65a17a23d025d1215d570097de0b
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-01 12:16:15 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.css"
-import { Home } from "./Components/home/home"
-import { Jobs } from "./Pages/jobs"
-import { Network } from "./Pages/network"
-import { Notifications } from "./Pages/notification"
-import { ProfilePage } from "./Pages/ProfilePage"
-// {/* <Home/> */}
-//       {/* <Network/> */}
-//       {/* <Jobs/> */}
-//       {/* <Notifications/> */}
+
+import { Routes } from "./Routes/Routes"
 
 function App() {
-
   return (
     <div className="app">
-      <ProfilePage />
+      <Routes />
     </div>
-
   )
 }
 
--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -1,56 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
-import Navbar from '../Components/Nav/Navbar'
-import ProfileCard from '../Components/FeedPage/ProfileCard'
-import CreatePost from '../Components/FeedPage/CreatePost'
-import PostCard from '../Components/FeedPage/PostCard'
-import NewsCard from '../Components/FeedPage/NewsCard'
-import CoursesCard from '../Components/FeedPage/CoursesCard'
-import {device} from '../Components/breakpoints'
+import React from "react"
+import styled from "styled-components"
+import Navbar from "../Components/Nav/Navbar"
+import ProfileCard from "../Components/FeedPage/ProfileCard"
+import CreatePost from "../Components/FeedPage/CreatePost"
+import PostCard from "../Components/FeedPage/PostCard"
+import NewsCard from "../Components/FeedPage/NewsCard"
+import CoursesCard from "../Components/FeedPage/CoursesCard"
+import { device } from "../Components/breakpoints"
 
 export default function FeedPage() {
-    return (
-        <>
-            <Navbar />
-            <Page>
-                <Left>
-                    <ProfileCard/>
-                </Left>
-                <Middle>
-                    <CreatePost />
-                    <PostCard/>
-                </Middle>
-                <Right>
-                    <NewsCard />
-                    <CoursesCard />
-                </Right>
-            </Page>
-        </>
-    )
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Page>
+        <Left>
+          <ProfileCard />
+        </Left>
+        <Middle>
+          <CreatePost />
+          <PostCard />
+        </Middle>
+        <Right>
+          <NewsCard />
+          <CoursesCard />
+        </Right>
+      </Page>
+    </>
+  )
 }
 
 //---------styled-components-----------
 
 const Page = styled.div`
-    display: flex;
-    justify-content:space-between;
-    margin: auto;
-    padding-top: 6.5rem;
-    max-width: 1256px;
-    /* @media ${device.desktop} {
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  padding-top: 6.5rem;
+  max-width: 1256px;
+  /* @media ${device.desktop} {
         max-width: 1256px;
     }
     @media ${device.laptopL} {
         max-width: 11560px;
     } */
-    
 `
-const Left = styled.div`
-
-`
-const Middle = styled.div`
-
-`
-const Right = styled.div`
-
-`
+const Left = styled.div``
+const Middle = styled.div``
+const Right = styled.div``
