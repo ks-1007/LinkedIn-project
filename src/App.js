0a500ef5d33a39abfc9c070ae038870958395f6f
Deependra-Negi <916deepaknegi@gmail.com>
2021-10-01 14:53:52 +0530

    join page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
+import Join from "./Components/JoinLoginForm/Join.jsx"
 
-import { Routes } from "./Routes/Routes"
+// import { Routes } from "./Routes/Routes"
 
 function App() {
   return (
     <div className="app">
-      <Routes />
+      {/* <Routes /> */}
+      <Join/>
     </div>
   )
 }
