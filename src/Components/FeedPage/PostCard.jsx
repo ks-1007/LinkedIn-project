c3430bbfcd9572e2a068fcd3de38c62e3941a2a0
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-03 15:00:31 +0530

    added feature to see users profile from feed also

--- src/Components/FeedPage/PostCard.jsx
+++ src/Components/FeedPage/PostCard.jsx
@@ -1,60 +1,63 @@
 import _ from "lodash"
 import React from "react"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 
 export default function PostCard({ user, body, pic }) {
   return (
     <CardCont>
-      <ProfileCont>
-        <UserImg>
-          <img
-            src={
-              user.profile_pic ||
-              "https://komarketing.com/images/2014/08/linkedin-default.png"
-            }
-            alt="profile"
-          />
-        </UserImg>
-        <Details>
-          <UserName>
-            <h3>{_.startCase(user.name)}</h3>
-          </UserName>
-          <Tagline>
-            <p>{_.startCase(user.description)}</p>
-          </Tagline>
-          <Time>
-            <p>10h</p>
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                data-supported-dps="16x16"
-                fill="#00000099"
-                class="mercado-match"
-                width="16"
-                height="16"
-                focusable="false"
-              >
-                <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
-              </svg>
-            </span>
-          </Time>
-        </Details>
-        <More>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            data-supported-dps="24x24"
-            fill="#00000099"
-            class="mercado-match"
-            width="24"
-            height="24"
-            focusable="false"
-          >
-            <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
-          </svg>
-        </More>
-      </ProfileCont>
+      <Link to={`/othersprofile/${user.email}`}>
+        <ProfileCont>
+          <UserImg>
+            <img
+              src={
+                user.profile_pic ||
+                "https://komarketing.com/images/2014/08/linkedin-default.png"
+              }
+              alt="profile"
+            />
+          </UserImg>
+          <Details>
+            <UserName>
+              <h3>{_.startCase(user.name)}</h3>
+            </UserName>
+            <Tagline>
+              <p>{_.startCase(user.description)}</p>
+            </Tagline>
+            <Time>
+              <p>10h</p>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  fill="#00000099"
+                  class="mercado-match"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
+                </svg>
+              </span>
+            </Time>
+          </Details>
+          <More>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              data-supported-dps="24x24"
+              fill="#00000099"
+              class="mercado-match"
+              width="24"
+              height="24"
+              focusable="false"
+            >
+              <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+            </svg>
+          </More>
+        </ProfileCont>
+      </Link>
       <CaptionCont>
         <p>{body}</p>
       </CaptionCont>
