6a07d29191473ea37497f3a7fbd93465df48f770
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-05 08:19:44 +0530

    changed link at join component

--- src/Components/JoinLoginForm/Join.jsx
+++ src/Components/JoinLoginForm/Join.jsx
@@ -27,7 +27,9 @@ export default function Join() {
         <div></div>
         <Btn>
           {" "}
-          <a href="http://localhost:5000/auth/google">Join with Google</a>{" "}
+          <a href="https://linked-in-project.vercel.app/auth/google">
+            Join with Google
+          </a>{" "}
         </Btn>
       </Form>
       <Help></Help>
