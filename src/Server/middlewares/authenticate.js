ed0544c6e8296903b3916de8574aa26bd787240b
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-06 10:50:11 +0530

    added like feature

--- src/Server/middlewares/authenticate.js
+++ src/Server/middlewares/authenticate.js
@@ -13,13 +13,13 @@ const verifyToken = (token) => {
 const authenticate = async function (req, res, next) {
   // bearerToken will be given from redux store
   const bearerToken = req?.headers?.authorization
-  console.log("bearerToken:", bearerToken)
+  // console.log("bearerToken:", bearerToken)
 
   const token = bearerToken.split(" ")[1]
-  console.log("token:", token)
+  // console.log("token:", token)
 
   const user = await verifyToken(token)
-  console.log("user:", user)
+  // console.log("user:", user)
 
   req.user = user
 
